b8a9352230467a85ab17c834ab8ae8cffbf83357
MelindaDini <melindadininingtyas@gmail.com>
2015-04-08 15:03:02 +0700

    Revert "Revert "tugas 3""
    This reverts commit c802ef9e305ac9d16ad6856ae4392f2430ad9731.

---
+++ Tugas 2/sepuluh.java
@@ -0,0 +1,16 @@
+public class sepuluh {
+	public static void main(String[] args) {
+		int s=7, l=9;
+		for (int h=1;h<=s ;h++ ) {
+			System.out.println(" ");
+			if (h%3==0) {
+				System.out.print(" #");
+				
+			}
+			for (int g=1;g<=l ;g++ ) {
+				System.out.print("#");
+				
+			}
+		}
+	}
+}
