5dbb8432485aa75181af02c1a8df7f1cca415cce
fachrezha <rezhakinan28@gmail.com>
2015-05-20 23:11:47 +0700

    Revert "Revert "tugas 5 pak""
    This reverts commit 31e335718c0a1072a50915517ca3aa75d28a22d1.

---
+++ Tugas 2/empat.java
@@ -0,0 +1,14 @@
+public class  empat {
+	public static void main(String[] args) {
+		int a;
+		for (a=1;a<11 ;a++ ) {
+			System.out.print(a);
+			if (a<10) {
+				System.out.print(",");
+				
+			}
+			
+		}
+	}
+	
+}
